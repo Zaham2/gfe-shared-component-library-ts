99457b8498761b83b50c129a36221e5700570327
Zaham2 <zaham2@yahoo.com>
2024-11-09 13:22:29 +0200

    First Commit With just the label component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Label from "./components/badge-component/Label"
+
+function App() {
+
+  return (
+    <>
+      <Label>Some label text</Label>
+    </>
+  )
+}
+
+export default App
---
+++ src/lib/contants.ts
@@ -0,0 +1,58 @@
+// --------------------
+// BADGE
+// --------------------
+export const LABEL_COLORS = {
+    GRAY: 'gray',
+    RED: 'red',
+    YELLOW: 'yellow',
+    GREEN: 'green',
+    BLUE: 'blue',
+}
+
+export const LABEL_SIZES = {
+    SMALL: 'small',
+    MEDIUM: 'medium',
+    LARGE: 'large',
+}
+
+// --------------------
+// BUTTON
+// --------------------
+const BUTTON_VARIANTS = {
+    PRIMARY: "primary",
+    SECONDARY: "secondary",
+    TERTIARY: "tertiary",
+    DESTRUCTIVE: "destructive",
+    LINK: "link",
+}
+
+const BUTTON_STATES = {
+    NORMAL: "-normal",
+    HOVER: "-hover",
+    ACTIVE: "-active",
+    DISABLED: "-disabled",
+    FOCUS: "-focus",
+}
+
+const BUTTON_SIZES = {
+    MEDIUM: "medium",
+    LARGE: "large",
+    XLARGE: "x-large",
+    TWO_X_LARGE: "two-x-large",
+}
+
+const ICONS = {
+    LEFT: "left",
+    RIGHT: "right",
+    BOTH_SIDES: "both-sides",
+    NONE: "none",
+
+}
+
+export {
+    BUTTON_VARIANTS,
+    BUTTON_STATES,
+    BUTTON_SIZES,
+    ICONS
+}
+// --------------------
