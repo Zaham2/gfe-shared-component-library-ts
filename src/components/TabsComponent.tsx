5159aebbeec40264a3a78a7045e7f7fb0c027189
Zaham2 <zaham2@yahoo.com>
2024-11-18 07:44:28 +0200

    Completed TabsComponent Component

--- src/components/TabsComponent.tsx
+++ src/components/TabsComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TABS_COMPONENT_STATES } from '../lib/constants'
+import { TABS_COMPONENT_STATES } from '../lib/constants';
 
 interface Props {
   tabs?: string[]
@@ -7,26 +7,18 @@ interface Props {
 
 const TabsComponent = (props: Props) => {
 
-  const [tabs, setTabs] = React.useState<string[]>([])
+  const [tabs, setTabs] = React.useState<string[]>([]);
   const [tabsStates, setTabsStates] = React.useState<string[]>([])
-  // const [activeTabBarWidth, setActiveTabBarWidth] = React.useState(0)
-  // const [activeTabBarOffset, setActiveTabBarOffset] = React.useState(0)
-  // const ref = React.useRef<HTMLDivElement>(null)
 
   React.useEffect(() => {
     const defaultTab = ['Tab 1']
     setTabs(props.tabs ?? defaultTab)
-    // setActiveTabBarWidth((1 / (props.tabs ?? defaultTab).length) * 100)
-    setTabsStates(Array.from({ length: tabs.length }, () => TABS_COMPONENT_STATES.INITIAL) ?? [TABS_COMPONENT_STATES.FOCUS])
-    // const { width } = ref.current?.getBoundingClientRect() ?? { width: 0 }
-    // const { width2 } = ref2.current?.getBoundingClientRect() ?? { width: 0 }
-
-    // setActiveTabBarOffset(width2 / (props.tabs))
+    setTabsStates(Array.from({ length: tabs.length }, (_, index) => index === 0 ? TABS_COMPONENT_STATES.FOCUS : TABS_COMPONENT_STATES.INITIAL) ?? [TABS_COMPONENT_STATES.FOCUS])
   }, [])
 
-  React.useEffect(() => {
+  // React.useEffect(() => {
 
-  }, [tabsStates])
+  // }, [tabsStates])
 
   const handleOnClick = (index: number) => {
     if (tabsStates[index] === TABS_COMPONENT_STATES.FOCUS) {
@@ -46,52 +38,31 @@ const TabsComponent = (props: Props) => {
   }
 
   return (
-    <div className="tabs-component-container">
+    <div className='tabs-component-container'>
       <div className="tabs-container">
-        <div className='tabs-full-bottom-bar'>
-          {
-            Array.from({ length: tabs.length }, (_, index) => (
-              <>
-                <button
-                  key={index}
-                  className={`tab`}
-                  onClick={() => handleOnClick(index)}
-                >
-                  {tabs[index]}
-                <div className='active-tab' style={{
-                  width: '100%',
-                }} />
-                </button>
-              </>
-
-            ))
-          }
-        </div>
-      </div>
-      {/* <div className='tabs-container'> */}
-      {/* {
-          Array.from({ length: tabs.length }, (_, index) => (
-            <>
-              <button
-                key={index}
-                className={`tab`}
-                onClick={() => handleOnClick(index)}
-              >
-                {tabs[index]}
-              </button>
+        {Array.from({ length: tabs.length }, (_, index) => (
+          <div className={`tab`}>
+            <div
+              key={index}
+              className={`tab-btn p-bottom-sm`}
+              onClick={() => handleOnClick(index)}
+            >
+              {tabs[index]}
+            </div>
+            {
+              tabsStates[index] === TABS_COMPONENT_STATES.FOCUS &&
               <div className='active-tab' style={{
-                width: '10%',
+                width: '100%',
               }} />
-            </>
-
-          ))
-        }
+            }
+          </div>
+        ))}
       </div>
-      <div ref={ref} className='tabs-full-bottom-bar m-top-xs'>
-      </div> */}
-      <p className="tabs-component-text p-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quaerat provident similique placeat, nesciunt eius itaque nulla deleniti esse iste quo in rem hic, et veniam magni error. Sit, et.</p>
+      <div className="tabs-full-bottom-bar" />
+      <p className="tabs-component-text p-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, maiores ipsum praesentium voluptatum molestiae unde blanditiis sapiente quam assumenda nesciunt similique consequuntur. Minus consequatur odit mollitia repudiandae fuga ipsum delectus.</p>
     </div>
   )
 }
 
 export default TabsComponent
+
